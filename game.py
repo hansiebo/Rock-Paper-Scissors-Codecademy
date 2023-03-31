2c003ddf353651b2fdb2d28632897c745cc2951a
Han Siebo <han.siebo@proton.me>
2023-03-31 12:46:15 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,11 @@
+# TIC TAC TOE GAME
+
+# INTRO | WELCOME
+print("Welcome to our Tic Tac Toe Game!"+ "\n")
+start = input("Type 'GO' to get started! " + "\n" + "\n")
+
+while (start != "GO"):
+  start = input("Awww, you don't want to play? That's ok. But if you do want to play, type 'GO' to get started." + "\n" + "\n")
+
+input("Awesome! Let's get started!")
+input("First, we need to create our first player")
